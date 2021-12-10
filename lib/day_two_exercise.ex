13528e93aa228b5df5b6fceed20265188cb1233d
Stanley Liu <sliu1993@hotmail.co.uk>
2021-12-10 22:35:40 +0000

    answer to day 2, part 2

--- lib/day_two_exercise.ex
+++ lib/day_two_exercise.ex
@@ -1,22 +1,65 @@
 defmodule DayTwoExercise do
   import Helpers
 
-  def calculate_position_and_depth(filename) do
+  def get_product_of_position_and_depth(filename) do
     read_file(filename)
     |> String.split("\n", trim: true)
     |> Enum.reduce(%{horizontal_position: 0, depth: 0}, fn x, acc ->
-          [direction, distance] = String.split(x, " ")
+      [direction, distance] = String.split(x, " ")
 
-          case {direction, distance} do
-            {"forward", distance} ->
-              %{horizontal_position: acc.horizontal_position + String.to_integer(distance), depth: acc.depth}
-            {"down", distance} ->
-              %{horizontal_position: acc.horizontal_position, depth: acc.depth + String.to_integer(distance)}
-            {"up", distance} ->
-              %{horizontal_position: acc.horizontal_position, depth: acc.depth - String.to_integer(distance)}
-          end
-       end)
+      case {direction, distance} do
+        {"forward", distance} ->
+          %{
+            horizontal_position: acc.horizontal_position + String.to_integer(distance),
+            depth: acc.depth
+          }
+
+        {"down", distance} ->
+          %{
+            horizontal_position: acc.horizontal_position,
+            depth: acc.depth + String.to_integer(distance)
+          }
+
+        {"up", distance} ->
+          %{
+            horizontal_position: acc.horizontal_position,
+            depth: acc.depth - String.to_integer(distance)
+          }
+      end
+    end)
     |> Map.values()
     |> Enum.product()
+  end
+
+  def get_correct_product(filename) do
+    read_file(filename)
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(%{horizontal_position: 0, depth: 0, aim: 0}, fn x, acc ->
+      [direction, distance] = String.split(x, " ")
+
+      case {direction, distance} do
+        {"forward", distance} ->
+          %{
+            horizontal_position: acc.horizontal_position + String.to_integer(distance),
+            depth: acc.depth + (acc.aim * String.to_integer(distance)),
+            aim: acc.aim
+          }
+
+        {"down", distance} ->
+          %{
+            horizontal_position: acc.horizontal_position,
+            depth: acc.depth,
+            aim: acc.aim + String.to_integer(distance)
+          }
+
+        {"up", distance} ->
+          %{
+            horizontal_position: acc.horizontal_position,
+            depth: acc.depth,
+            aim: acc.aim - String.to_integer(distance)
+          }
+      end
+    end)
+    |> (&(&1.horizontal_position * &1.depth)/1).()
   end
 end
