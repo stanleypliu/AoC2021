c3f136d1778a723cb9c1e3441f1e9573e5162ad5
Stanley Liu <sliu1993@hotmail.co.uk>
2021-12-10 20:29:49 +0000

    answer to part 2, day 1

--- lib/day_one_exercise.ex
+++ lib/day_one_exercise.ex
@@ -14,11 +14,22 @@ defmodule DayOneExercise do
   def count_increases(filename) do
     list_of_measurements = parse_file_into_numbers_list(filename)
 
-    map_of_counts = Enum.reduce(list_of_measurements, %{last_num: nil, count: 0, values: [List.first(list_of_measurements)]}, fn x, acc ->
-      new_count = if x > List.last(acc.values), do: acc.count + 1, else: acc.count
-      %{last_num: x, values: acc.values ++ [x], count: new_count}
-    end
-    )
+    count_consecutive_increases(list_of_measurements, &Function.identity/1)
+  end
+
+  def measurement_window_comparison(filename) do
+    list_of_measurements = parse_file_into_numbers_list(filename)
+
+    chunked_list = Enum.chunk_every(list_of_measurements, 3, 1)
+
+    count_consecutive_increases(chunked_list, &Enum.sum/1)
+  end
+
+  def count_consecutive_increases(list, transform) do
+    map_of_counts = Enum.reduce(list, %{last_elem: nil, count: 0, values: [List.first(list)]}, fn current_elem, acc ->
+      new_count = if transform.(current_elem) > transform.(List.last(acc.values)), do: acc.count + 1, else: acc.count
+      %{last_elem: current_elem, values: acc.values ++ [current_elem], count: new_count}
+    end)
 
     map_of_counts.count
   end
