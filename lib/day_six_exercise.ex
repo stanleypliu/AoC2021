a72272a66007f2cedf65e0b13bafa1c0cdfafa31
Stanley Liu <sliu1993@hotmail.co.uk>
2021-12-19 21:19:21 +0000

    answer to day 6, part 2, courtesy of Jose Valim

--- lib/day_six_exercise.ex
+++ lib/day_six_exercise.ex
@@ -46,4 +46,25 @@ defmodule DaySixExercise do
         fish_reproduction_cycle(updated_population, days, counter + 1)
     end
   end
+
+  # Going with José Valim's solution for part 2 due to its performance
+  def faster_solution(filename, days) do
+    original_fish =
+      read_file(filename)
+      |> String.split(",", trim: true)
+      |> Enum.map(&String.to_integer/1)
+
+    frequencies = Enum.frequencies(original_fish)
+    amounts = Enum.map(0..8, fn i -> frequencies[i] || 0 end) |> List.to_tuple()
+
+    if days > 1 do
+      1..days
+      |> Enum.reduce(amounts, fn _, amounts -> recurse(amounts) end)
+      |> Tuple.sum()
+    end
+  end
+
+  def recurse({day0, day1, day2, day3, day4, day5, day6, day7, day8}) do
+    {day1, day2, day3, day4, day5, day6, day7 + day0, day8, day0}
+  end
 end
